1fab434dff320a9ab699044be5978a70f07431fa
Ben Basty <benbasty@yahoo.fr>
2021-10-03 21:21:46 +0800

    Create tictactoe.java

---
+++ Stage 1/tictactoe.java
@@ -0,0 +1,14 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        print();
+    }
+
+    private static void print() {
+        System.out.println("X O X");
+        System.out.println("O X O");
+        System.out.println("X X O");
+    }
+}
